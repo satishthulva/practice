50b9e3fcd66216ec27753487e3a34b21790c6a66
satishthulva <sathya691@gmail.com>
2018-10-02 12:32:05 +0530

    optimised alog

--- practice/first/SubArraysMinSum.java
+++ practice/first/SubArraysMinSum.java
@@ -9,6 +9,8 @@
  * Subarrays : [4], [6], [8], [9], [1], [4, 6], [6, 8], [8, 9], [9, 1], [4, 6, 8], [6, 8, 9], [8, 9, 1], [4, 6, 8, 9], [6, 8, 9, 1]
  * Minimum	 :  4    6    8    9    1      4       6       8       1        4          6          1            4            1
  * Sum 		 :  63
+ * 
+ * Brute force is O(n ^ 3) and divide and conquer is O(n ^ 2) solution
  * </pre>
  * @author rebecca
  */
@@ -54,11 +56,53 @@ public class SubArraysMinSum {
 		
 		return subArrayMinSum;
 	}
+
+	private static long findSubArrayMinSumDivideAndConquer(int[] arr, int startIndex, int endIndex) {
+		if(startIndex > endIndex || endIndex > arr.length - 1)
+			return 0;
+		
+		if(startIndex == endIndex)
+			return arr[startIndex];
+
+		long minSum = 0;
+		// total three subarrays --> min elem will come twice
+		if(startIndex + 1 == endIndex) {
+			minSum+=arr[startIndex];
+			if(minSum < arr[endIndex])
+				minSum+=arr[startIndex] + arr[endIndex];
+			else
+				minSum+= 2 * arr[endIndex];
+			
+			return minSum;
+		}
+		
+		int minElem = arr[startIndex], minElemIndex = startIndex;
+		for(int i = startIndex; i <= endIndex; i+=1) {
+			if(minElem > arr[i]) {
+				minElem = arr[i];
+				minElemIndex = i;
+			}
+		}
+		
+		return minElem * (minElemIndex - startIndex + 1) * (endIndex - minElemIndex + 1) + 
+			findSubArrayMinSumDivideAndConquer(arr, startIndex, minElemIndex - 1) + 
+			findSubArrayMinSumDivideAndConquer(arr, minElemIndex + 1, endIndex);
+	}
+	
+	public static long findSubArrayMinSumDivideAndConquerHelper(int[] arr) {
+		long includingMainArray = findSubArrayMinSumDivideAndConquer(arr, 0, arr.length - 1);
+		int min = arr[0];
+		for(int num : arr) {
+			min = min > num ? num : min;
+		}
+		return includingMainArray - min;
+	}
 	
 	public static void main(String[] args)
 	{
 		int[] arr = {4, 6, 8, 9, 1};
 		System.out.println(findSubArrayMinSum(arr));
+		System.out.println(findSubArrayMinSumDivideAndConquerHelper(arr));
 	}
 	
 }
